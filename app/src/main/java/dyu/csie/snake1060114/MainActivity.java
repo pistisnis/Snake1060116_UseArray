c365bba7e6ece38486a06049b59e55915224ea6c
unknown <Alpha@Alpha-PC.dyu.edu.tw>
2017-01-21 21:46:52 +0800

    Initial commit

---
+++ app/src/main/java/dyu/csie/snake1060114/MainActivity.java
@@ -0,0 +1,14 @@
+package dyu.csie.snake1060114;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        setContentView( new SnakeView(this) );
+    }
+}
